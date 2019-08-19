1386de742bb0518da1e6e7aae25b30d795487585
sselvarasux <sselvarasux@SSelvarasuW8.eagleinvsys.com>
2019-08-19 15:02:41 +0530

    first program

--- MavenProject1/src/main/java/myFirstPackage/FirstProgram.java
+++ MavenProject1/src/main/java/myFirstPackage/FirstProgram.java
@@ -3,15 +3,11 @@ package myFirstPackage;
 public class FirstProgram {
 
 	public static void main(String[] args) {
-		int num = 18;
-		if (num % 2 == 0) {
-			System.out.println("Given number is not prime");
-		}
-		else {
-			System.out.println("This is prime");
-		}
 		
-
+			System.out.println("Welcome to Java world");
+			System.out.println("This is sample program");
+		
+		
 	}
 
 }
