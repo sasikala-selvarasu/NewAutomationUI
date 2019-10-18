b88162e8766d964e847f551f043510eec2823ce5
sselvarasux <sselvarasux@SSelvarasuW8.eagleinvsys.com>
2019-10-18 18:17:26 +0530

    Oct18-latestchanges-evening

---
+++ MavenProject1/src/main/java/testcases/ChartsPage_Testcases.java
@@ -0,0 +1,65 @@
+package testcases;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import pages.LoginPage;
+import pages.ResultPage;
+import wrappers.Annotations;
+
+import pages.ChartsPage;
+import pages.HomePage;
+
+import pages.ReportResultPage;
+import pages.ReportTabpage;
+
+//import wrappers.Annotations;
+
+public class ChartsPage_Testcases extends Annotations {
+	Boolean bEditMode;
+
+	@BeforeTest
+	public void setData() {
+		excelFileName = "ChartsPage";
+
+	}
+
+	@Test(dataProvider = "fetchData")
+	public void ChartsPage(String Url, String userName, String password, String ReportNames, String ExpectedStrs, String fileWithPath)
+			throws InterruptedException, IOException {
+
+		new LoginPage().enterUrl(Url).enterUserName(userName).enterPassword(password).clickLoginButton();
+
+		String[] result = ReportNames.split(",");
+		String[] expResults = ExpectedStrs.split(",");
+//		String[] TimeOut = Timeouts.split(",");
+		
+//		System.out.println (result);
+//		System.out.println (expResults);
+//		System.out.println (TimeOut);
+		
+		for (int x = 0; x < result.length; x++) {
+
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			new HomePage()
+			.ClickOnInbox()
+			.ClickonReport(result[x])
+			.ClickonAnalyse();
+			
+			new ResultPage()
+			 .CheckTextCommentary(expResults[x])
+			 .ClickGoCharts();
+
+			new ChartsPage()
+			 .ClickCharts(fileWithPath);
+ 
+			
+
+			 driver.close();
+			// }
+		}
+	}
+}
