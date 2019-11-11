1f87896c7d4ff3cf108fcd431eaa210e5f05e21f
sselvarasux <sselvarasux@SSelvarasuW8.eagleinvsys.com>
2019-11-11 12:51:19 +0530

    Checked into missed testcases xml and excel file

---
+++ MavenProject1/src/main/java/testcases/Schw15r2dw003_Testcases.java
@@ -0,0 +1,90 @@
+package testcases;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import pages.LoginPage;
+import pages.ResultPage;
+import wrappers.Annotations;
+
+import pages.ChartsPage;
+import pages.HomePage;
+
+import pages.ReportResultPage;
+import pages.ReportTabpage;
+
+//import wrappers.Annotations;
+
+public class Schw15r2dw003_Testcases extends Annotations {
+	Boolean bEditMode;
+
+	@BeforeTest
+	public void setData() {
+		excelFileName = "Testcases_for_schw15r2dw003";
+
+	}
+
+	@Test(dataProvider = "fetchData")
+	public void loginLogout(String Url, String userName, String password, String ReportNames, String ExpectedStrs,
+			String Timeouts, String fileWithPath)
+			throws InterruptedException, IOException {
+
+		new LoginPage().enterUrl(Url).enterUserName(userName).enterPassword(password).clickLoginButton();
+
+		String[] result = ReportNames.split(",");
+		String[] expResults = ExpectedStrs.split(",");
+		String[] TimeOut = Timeouts.split(",");
+		
+//		System.out.println (result);
+//		System.out.println (expResults);
+//		System.out.println (TimeOut);
+		
+		for (int x = 0; x < result.length; x++) {
+
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			new HomePage()
+			.ClickOnInbox()
+			.ClickonReport(result[x])
+			.ClickonAnalyse();
+
+			// Thread.sleep(Integer.parseInt(TimeOut[x]) );
+
+		//	 new LogicBuilder()
+			// new LogicBuilder2()
+		//	 .doCalculation()
+		//	 .LogicbuilderPage();
+
+			 new ResultPage()
+			 .CheckTextCommentary(expResults[x])
+			 .ClickGoCharts();
+			 
+			 
+			 //.CloseReportTab();
+
+			 new ChartsPage()
+			 .ClickCharts(fileWithPath);
+
+//			 new ReportTabpage()
+//			 .ClickOnReports()
+//			 .ClickOnReportTabReport(ReportsTabReport);
+//			  bEditMode = true;
+//			 new ReportResultPage()
+//			 .CommentarySetGet(bEditMode, URL, Key);
+
+			// Thread.sleep(Integer.parseInt(TimeOut[x]) );
+
+			// new ReportTabpage()
+			// .ClickOnReports()
+			// .ClickOnReportTabReport(ReportsTabReport);
+			// bEditMode = false;
+			// new ReportResultPage()
+//			.CommentarySetGet(bEditMode, URL, Key);
+
+			 driver.close();
+			// }
+		}
+	}
+}
